81b2de943e7029170ecb5c3501badd994858fc1a
avoska <dimakoncha@gmail.com>
2016-03-10 15:27:56 +0300

    first commit

---
+++ js/open.js
@@ -0,0 +1,189 @@
+function open_armatura_func() {
+	$('#slide_panel_balka').hide();
+	$('#slide_panel_polosa').hide();
+	$('#slide_panel_provolka').hide();
+	$('#slide_panel_ygolok').hide();
+	$('#slide_panel_shvell').hide();
+	$('#slide_panel_prokat').hide();
+	$('#slide_panel_truba_kv').hide();
+	$('#slide_panel_truba_kr').hide();
+	$('#slide_panel_truba_prof').hide();
+	$('#slide_panel_nastil').hide();
+	$('#slide_panel_setka').hide();
+	$('#slide_panel_arma').fadeIn('slow');
+	$("html,body").animate({scrollTop: $(slide_panel_arma).offset().top}, 1000)
+}
+function open_balka_func() {
+	$('#slide_panel_arma').hide();
+	$('#slide_panel_polosa').hide();
+	$('#slide_panel_provolka').hide();
+	$('#slide_panel_ygolok').hide();
+	$('#slide_panel_shvell').hide();
+	$('#slide_panel_prokat').hide();
+	$('#slide_panel_truba_kv').hide();
+	$('#slide_panel_truba_kr').hide();
+	$('#slide_panel_truba_prof').hide();
+	$('#slide_panel_nastil').hide();
+	$('#slide_panel_setka').hide();
+	$('#slide_panel_balka').fadeIn('slow');
+	$("html,body").animate({scrollTop: $(slide_panel_balka).offset().top}, 1000)
+}
+function open_polosa_func() {
+	$('#slide_panel_arma').hide();
+	$('#slide_panel_balka').hide();
+	$('#slide_panel_provolka').hide();
+	$('#slide_panel_ygolok').hide();
+	$('#slide_panel_shvell').hide();
+	$('#slide_panel_prokat').hide();
+	$('#slide_panel_truba_kv').hide();
+	$('#slide_panel_truba_kr').hide();
+	$('#slide_panel_truba_prof').hide();
+	$('#slide_panel_nastil').hide();
+	$('#slide_panel_setka').hide();
+	$('#slide_panel_polosa').fadeIn('slow');
+	$("html,body").animate({scrollTop: $(slide_panel_polosa).offset().top}, 1000)
+}
+function open_provolka_func() {
+	$('#slide_panel_arma').hide();
+	$('#slide_panel_balka').hide();
+	$('#slide_panel_polosa').hide();
+	$('#slide_panel_ygolok').hide();
+	$('#slide_panel_shvell').hide();
+	$('#slide_panel_prokat').hide();
+	$('#slide_panel_truba_kv').hide();
+	$('#slide_panel_truba_kr').hide();
+	$('#slide_panel_truba_prof').hide();
+	$('#slide_panel_nastil').hide();
+	$('#slide_panel_setka').hide();
+	$('#slide_panel_provolka').fadeIn('slow');
+	$("html,body").animate({scrollTop: $(slide_panel_provolka).offset().top}, 1000)
+}
+function open_ygolok_func() {
+	$('#slide_panel_arma').hide();
+	$('#slide_panel_balka').hide();
+	$('#slide_panel_polosa').hide();
+	$('#slide_panel_provolka').hide();
+	$('#slide_panel_shvell').hide();
+	$('#slide_panel_prokat').hide();
+	$('#slide_panel_truba_kv').hide();
+	$('#slide_panel_truba_kr').hide();
+	$('#slide_panel_truba_prof').hide();
+	$('#slide_panel_nastil').hide();
+	$('#slide_panel_setka').hide();
+	$('#slide_panel_ygolok').fadeIn('slow');
+	$("html,body").animate({scrollTop: $(slide_panel_ygolok).offset().top}, 1000)
+}
+function open_shvell_func() {
+	$('#slide_panel_arma').hide();
+	$('#slide_panel_balka').hide();
+	$('#slide_panel_polosa').hide();
+	$('#slide_panel_provolka').hide();
+	$('#slide_panel_ygolok').hide();
+	$('#slide_panel_prokat').hide();
+	$('#slide_panel_truba_kv').hide();
+	$('#slide_panel_truba_kr').hide();
+	$('#slide_panel_truba_prof').hide();
+	$('#slide_panel_nastil').hide();
+	$('#slide_panel_setka').hide();
+	$('#slide_panel_shvell').fadeIn('slow');
+	$("html,body").animate({scrollTop: $(slide_panel_shvell).offset().top}, 1000)
+}
+function open_prokat_func() {
+	$('#slide_panel_arma').hide();
+	$('#slide_panel_balka').hide();
+	$('#slide_panel_polosa').hide();
+	$('#slide_panel_provolka').hide();
+	$('#slide_panel_ygolok').hide();
+	$('#slide_panel_shvell').hide();
+	$('#slide_panel_truba_kv').hide();
+	$('#slide_panel_truba_kr').hide();
+	$('#slide_panel_truba_prof').hide();
+	$('#slide_panel_nastil').hide();
+	$('#slide_panel_setka').hide();
+	$('#slide_panel_prokat').fadeIn('slow');
+	$("html,body").animate({scrollTop: $(slide_panel_prokat).offset().top}, 1000)
+}
+function open_truba_kv_func() {
+	$('#slide_panel_arma').hide();
+	$('#slide_panel_balka').hide();
+	$('#slide_panel_polosa').hide();
+	$('#slide_panel_provolka').hide();
+	$('#slide_panel_ygolok').hide();
+	$('#slide_panel_shvell').hide();
+	$('#slide_panel_prokat').hide();
+	$('#slide_panel_truba_kr').hide();
+	$('#slide_panel_truba_prof').hide();
+	$('#slide_panel_nastil').hide();
+	$('#slide_panel_setka').hide();
+	$('#slide_panel_truba_kv').fadeIn('slow');
+	$("html,body").animate({scrollTop: $(slide_panel_truba_kv).offset().top}, 1000)
+}
+function open_truba_kr_func() {
+	$('#slide_panel_arma').hide();
+	$('#slide_panel_balka').hide();
+	$('#slide_panel_polosa').hide();
+	$('#slide_panel_provolka').hide();
+	$('#slide_panel_ygolok').hide();
+	$('#slide_panel_shvell').hide();
+	$('#slide_panel_prokat').hide();
+	$('#slide_panel_truba_kv').hide();
+	$('#slide_panel_truba_prof').hide();
+	$('#slide_panel_nastil').hide();
+	$('#slide_panel_setka').hide();
+	$('#slide_panel_truba_kr').fadeIn('slow');
+	$("html,body").animate({scrollTop: $(slide_panel_truba_kr).offset().top}, 1000)
+}
+function open_truba_prof_func() {
+	$('#slide_panel_arma').hide();
+	$('#slide_panel_balka').hide();
+	$('#slide_panel_polosa').hide();
+	$('#slide_panel_provolka').hide();
+	$('#slide_panel_ygolok').hide();
+	$('#slide_panel_shvell').hide();
+	$('#slide_panel_prokat').hide();
+	$('#slide_panel_truba_kv').hide();
+	$('#slide_panel_truba_kr').hide();
+	$('#slide_panel_nastil').hide();
+	$('#slide_panel_setka').hide();
+	$('#slide_panel_truba_prof').fadeIn('slow');
+	$("html,body").animate({scrollTop: $(slide_panel_truba_prof).offset().top}, 1000)
+}
+function open_nastil_func() {
+	$('#slide_panel_arma').hide();
+	$('#slide_panel_balka').hide();
+	$('#slide_panel_polosa').hide();
+	$('#slide_panel_provolka').hide();
+	$('#slide_panel_ygolok').hide();
+	$('#slide_panel_shvell').hide();
+	$('#slide_panel_prokat').hide();
+	$('#slide_panel_truba_kv').hide();
+	$('#slide_panel_truba_kr').hide();
+	$('#slide_panel_truba_prof').hide();
+	$('#slide_panel_setka').hide();
+	$('#slide_panel_nastil').fadeIn('slow');
+	$("html,body").animate({scrollTop: $(slide_panel_nastil).offset().top}, 1000)
+}
+function open_setka_func() {
+	$('#slide_panel_arma').hide();
+	$('#slide_panel_balka').hide();
+	$('#slide_panel_polosa').hide();
+	$('#slide_panel_provolka').hide();
+	$('#slide_panel_ygolok').hide();
+	$('#slide_panel_shvell').hide();
+	$('#slide_panel_prokat').hide();
+	$('#slide_panel_truba_kv').hide();
+	$('#slide_panel_truba_kr').hide();
+	$('#slide_panel_truba_prof').hide();
+	$('#slide_panel_nastil').hide();
+	$('#slide_panel_setka').fadeIn('slow');
+	$("html,body").animate({scrollTop: $(slide_panel_setka).offset().top}, 1000)
+}
+function open_office() {
+	$('#slide_panel_office').fadeToggle('slow');
+	$("html,body").animate({scrollTop: $(slide_panel_office).offset().top}, 1000)
+}
+function open_sklad() {
+	$('#slide_panel_sklad').fadeToggle('slow');
+	$('#slide_panel_sklad_2').fadeToggle('slow');
+	$("html,body").animate({scrollTop: $(slide_panel_sklad).offset().top}, 1000)
+}
